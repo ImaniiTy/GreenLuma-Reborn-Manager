aebf77a8a5b3cd020e5143bfe2b87f6eb7f5064b
ImaniiTy <augusto.vieira@dcomp.ufs.br>
2019-04-23 13:10:03 -0300

    updated the way that the .ini is handled(in a less lazy way...)

--- core.py
+++ core.py
@@ -8,7 +8,7 @@ from requests.exceptions import ConnectionError, ConnectTimeout
 
 BASE_PATH = "{}/GLR_Manager".format(os.getenv("LOCALAPPDATA"))
 PROFILES_PATH = "{}/Profiles".format(BASE_PATH)
-CURRENT_VERSION = "1.2.2"
+CURRENT_VERSION = "1.2.5"
 
 class Game:
     def __init__(self,id,name,type):
